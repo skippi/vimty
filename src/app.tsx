9bd55be97a05617bdb7811bf927fe880d02030e5
skippi <jbtcao@gmail.com>
2020-09-18 21:53:38 -0500

    Implemented basic react webpack config

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+function App(_: any) {
+  return <div>hello world</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
