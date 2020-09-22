dc04cfe7e7042b052f4c348a37fbb0c5d2b07d41
skippi <jbtcao@gmail.com>
2020-09-21 23:47:51 -0500

    Changed word to reset if typed wrong

--- src/app.tsx
+++ src/app.tsx
@@ -9,36 +9,47 @@ const MOTIONS = [
   'gg', 'w', 'W', 'e', 'E', 'b', 'B', 'ge', 'gE'
 ]
 
-function makePrompt() {
+const OPERATIONS = (() => {
   const ops: string[] = []
   for (let i = 0; i < 50; ++i) {
     const operator = OPERATORS[Math.floor(Math.random() * OPERATORS.length)]
     const motion = MOTIONS[Math.floor(Math.random() * MOTIONS.length)]
     ops.push(operator.concat(motion))
   }
-  return ops.join(" ")
-}
+  return ops
+})()
 
 function App(_: {}) {
-  const [text] = useState(makePrompt())
-  const [position, setPosition] = useState(0)
+  const [opIndex, setOpIndex] = useState(0)
+  const [charIndex, setCharIndex] = useState(0)
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key == text.charAt(position)) {
-        setPosition(position + 1)
+      const op = OPERATIONS[opIndex]
+      if (event.key == "Shift") return
+      if (event.key == op.charAt(charIndex)) {
+        setCharIndex(charIndex + 1)
+      } else {
+        setCharIndex(0)
+      }
+      if (charIndex >= op.length - 1) {
+        setCharIndex(0)
+        setOpIndex(opIndex + 1)
       }
     }
     document.addEventListener("keydown", handleKeyDown)
     return () => { document.removeEventListener("keydown", handleKeyDown) }
   })
-  return <Prompt text={text} position={position} />
+  return <Prompt
+    current={OPERATIONS[opIndex].slice(charIndex)}
+    remaining={OPERATIONS.slice(opIndex + 1)}
+  />
 }
 
-function Prompt(props: {text: string, position: number}) {
-  const { text, position } = props
+function Prompt(props: {current: string, remaining: string[]}) {
+  const { current, remaining } = props
   return <div>
-    <span style={{color: "red"}}>{text.slice(0, position)}</span>
-    <span>{text.slice(position)}</span>
+    <div style={{color: "red"}}>{current}</div>
+    <div>{remaining.join(" ")}</div>
   </div>
 }
 
